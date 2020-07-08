6a8d2d6c128d668f3597bc3fb6f88bf0cb31a4da
Tony Huang <tonyth00@gmail.com>
2020-07-07 17:24:34 -0700

    init commit

---
+++ android/app/src/main/kotlin/com/thuang/mi_card_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thuang.mi_card_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/tony.jpg'),
+              ),
+              Text('Tony Huang', style: TextStyle(fontSize: 40.0, color: Colors.white, fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
